44cbfd7ef09ebe90e5d0ae46926fcfb583f72365
milanbukvic <milanbukvic2017@gmail.com>
2018-09-17 00:43:41 +0200

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDewKApWMdIq4ZGAkaWYJOKsHyShIAmZm0",
+    authDomain: "todolistapp-b4747.firebaseapp.com",
+    databaseURL: "https://todolistapp-b4747.firebaseio.com",
+    projectId: "todolistapp-b4747",
+    storageBucket: "todolistapp-b4747.appspot.com",
+    messagingSenderId: "210273162401"
+  }
+};
